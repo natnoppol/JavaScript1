5a39957fdf62ab516f8dd64126fbf1a8b0b35822
natnoppol <126169607+natnoppol@users.noreply.github.com>
2024-01-23 23:21:40 +0100

    header
    header

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 const API_KEY = '6265479d138338f112ad9783ca7d0d06'
-const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1`;
+const apiUrl = `https://api.noroff.dev/api/v1/square-eyes`;
 const IMAGEPATH = 'https://image.tmdb.org/t/p/w500/';
 
 const main = document.querySelector('#main');
 
-function showMovies (url) {
-    fetch(url)
+function showMovies () {
+
+    fetch(apiUrl)
     .then(res => res.json())
-    .then(function(data){
-        console.log(data);
-        data.results.forEach(element=>{
-            const el= document.createElement('div');
-            const image= document.createElement('img');
-            const text= document.createElement('h2');
+    .then(json => console.log(json))
+    .catch(error => console.log(error)) // handle the error
+
+    // fetch(url)
+    
+    // .then(function(data){
+    //     console.log(data);
+    //     data.results.forEach(element=>{
+    //         const el= document.createElement('div');
+    //         const image= document.createElement('img');
+    //         const text= document.createElement('h2');
             
-            text.innterHTML = `${element.title}`;
-            image.src = IMAGEPATH + element.poster_path;
-            el.appendChild(image);
-            el.appendChild(text);
-            main.appendChild(el);
-        })
-    })
+    //         text.innterHTML = `${element.title}`;
+    //         image.src = IMAGEPATH + element.poster_path;
+    //         el.appendChild(image);
+    //         el.appendChild(text);
+    //         main.appendChild(el);
+    //     })
+    // })
 }
+
+function movie () 
+
