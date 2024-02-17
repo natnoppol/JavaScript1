16c7b28543b820ca473f6b67f351dc2e1ecfea49
same_des <eightsbinary@gmail.com>
2024-02-17 13:04:13 +0700

    remove: unused code and add: event to render when DOMLoaded

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const getRainyday = async () => {
-    const res = await fetch("https://api.noroff.dev/api/v1/rainy-days");
-    
-    return res.json();
-}
+  const res = await fetch('https://api.noroff.dev/api/v1/rainy-days');
+
+  return res.json();
+};
 
 const el = async () => {
-    const data = await getRainyday()
-    const contHome = document.querySelector('#data-container-cloth')
-    const productItem = data.map(post =>
-
-        `
-         <a href="/product/index.html?id=${post.id}">
+  const data = await getRainyday();
+  const contHome = document.querySelector('#data-container-cloth');
+  const productItem = data.map(
+    (post) =>
+      `
+            <a href="/product/index.html?id=${post.id}">
             <div class="product-wrapper">
                 <div class="product-item">
                     <img src="${post.image}"></img>
@@ -21,88 +21,12 @@ const el = async () => {
                 </div>
             </div>
         </a>
-       
+        
         `
+  );
+  contHome.innerHTML = productItem.join('');
+};
 
-    )
-    contHome.innerHTML = productItem.join('')
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import cart from "./js/script";
-// import products from "./js/product";
-
-
-// let app = document.getElementById('app');
-// let temporaryContent = document.getElementById('temporaryContent');
-
-// const loadTemplate = () => {
-//     fetch('/template.html')
-//         .then(response => response.text())
-//         .then(html => {
-//             app.innerHTML = html;
-//             let contentTab = document.getElementById('contentTab');
-//             contentTab.innerHTML = temporaryContent.innerHTML;
-//             temporaryContent.innerHTML = null;
-//             cart()
-//             initApp();
-//         })
-// }
-
-// loadTemplate();
-// const initApp = () => {
-//     let listProduct = document.querySelector('.listProduct');
-//     listProduct.innerHTML = null
-//     products.foreach (product=>{
-//         let newProduct = document.createElement('div');
-//         newProduct.classList.add('item');
-//         newProduct.innerHTML = 
-//         `
-//         <img src="${product.image}"/>
-//         <h2>${product.name}</h2>
-//         <div class="price">${product.price}</div>>
-//         `;
-//         listProduct.appendChild(newProduct)
-//     })
-// }
+document.addEventListener('DOMContentLoaded', async function () {
+  await el();
+});
