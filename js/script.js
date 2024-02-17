6123877ffceae79e1c1284387e8dfe126ffab83d
same_des <eightsbinary@gmail.com>
2024-02-17 13:20:12 +0700

    fix: read the list below - remove: searchApi - add: media query check on element that only appear on small device - fix: change var to const to set proper scope - update: querySelector related to HTML - fix: not setting the value related to define variable - add: media query check before call the function

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
-const dataContainer = document.getElementById("data-container-cloth");
-const searchApi = ``;
+const dataContainer = document.getElementById('data-container-cloth');
 
-window.addEventListener("scroll", function () {
-    var header = this.document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 50)
-})
+// Define the media query
+const mediaQuery = window.matchMedia('(max-width: 768px)');
 
-var navBar = document.getElementById("navBar")
+window.addEventListener('scroll', function () {
+  const header = this.document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 50);
+});
+
+const navBar = document.getElementById('navBar');
 
 function showMenu() {
-    const elem = document.querySelector('.fas-bars');
-    elem.addEventListener('click', function () {
-        navBar.style.left = "-15px";
-    });
+  const elem = document.querySelector('.fa-bars');
+  elem.addEventListener('click', function () {
+    navBar.style.left = '-15px';
+  });
 }
 
 function hideMenu() {
-    const elem = document.querySelector('.fa-times');
-    elem.addEventListener('click', function () {
-        navBar.style.left = "-300px";
-    });
+  const elem = document.querySelector('.fa-times');
+  elem.addEventListener('click', function () {
+    navBar.style.left = '-300px';
+  });
 }
 
-//update the number on cart relate to the item in the cart 
+//update the number on cart relate to the item in the cart
 function updateCartAmount() {
-    const countCartItem = document.getElementById('countCartItems');
-    const cartItemAmount = JSON.parse(localStorage.getItem('cart')) || [];
-    countCartItem.textContent.length;
+  const countCartItem = document.getElementById('countCartItems');
+  const cartItemAmount = JSON.parse(localStorage.getItem('cart')) || [];
+  countCartItem.textContent = cartItemAmount.length;
 }
 document.addEventListener('DOMContentLoaded', () => {
+  if (mediaQuery) {
     hideMenu();
     showMenu();
-    updateCartAmount();
+  }
+  updateCartAmount();
 });
-
